4aa9c496f2c7dd94a0fcae84ea5fab9c67843327
OSSome01 <omkarsargar01@gmail.com>
2020-09-12 10:56:06 +0530

    add robot decription

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ossome/slam_ws/src
+# Build directory: /home/ossome/slam_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("slamBot_control")
+subdirs("slamBot_description")
+subdirs("slamBot_gazebo")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ossome/slam_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ossome/slam_ws/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/ossome/slam_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ossome/slam_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ossome/slam_ws/src:$ROS_PACKAGE_PATH"
