c4453010d4cecf1f595f09fa9234b6cf97693969
OSSome01 <omkarsargar01@gmail.com>
2020-09-13 14:49:25 +0530

    update .gitignore

--- build/slamBot_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ossome/slam_ws/src/slamBot_gazebo
-# Build directory: /home/ossome/slam_ws/build/slamBot_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/slamBot_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ossome/slam_ws/src/slamBot_navigation
-# Build directory: /home/ossome/slam_ws/build/slamBot_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
